349f9956c783ff5eb694386d305dabebc97ab09b
V1cent <vincent.delacour@iepg.fr>
2019-12-20 13:15:10 +0100

    js for run game done

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,7 +12,6 @@ const $ = require("jquery");
 var app = {
   init: function() {
     $(".choices").on("click", app.choose);
-    console.log("i m here");
   },
   choose: function() {
     var choice = $(this).data("build");
@@ -20,8 +19,26 @@ var app = {
     var answerAlt = $("#answer").data("alt");
 
     if (choice === answer || choice === answerAlt) {
-      $("#answer").removeClass("d-none");
-      
+      $("#answer")
+        .children()
+        .show();
+
+      setTimeout(() => {
+        $("#answer")
+          .children()
+          .hide();
+
+        var item1 = Math.floor(Math.random() * 8) + 1;
+        var item2 = Math.floor(Math.random() * 8) + 1;
+        var answer = parseInt(item1 + "" + item2);
+        var alt = parseInt(item2 + "" + item1);
+
+        $("#item_1").attr("class", "item" + item1);
+        $("#item_2").attr("class", "item" + item2);
+        $("#answer").data("answer", answer);
+        $("#answer").data("alt", alt);
+        $("#answer_image").attr("class", "build" + answer);
+      }, 3000);
     } else {
       alert("Arf... ce n'est pas ça");
     }
